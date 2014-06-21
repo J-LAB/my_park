7851455c172da73a97967f174da01306f476cac3
johnballantyne@gmail.com <john@john-ubuntu.(none)>
2014-06-21 13:28:56 -0400

    Display trails data

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,9 +1,18 @@
 
 window.addEventListener('load', function() {
+
     var map = new OpenLayers.Map( 'map' );
     var layer = new OpenLayers.Layer.WMS( "OpenLayers WMS", 
             "http://vmap0.tiles.osgeo.org/wms/vmap0",
             {layers: 'basic'} );
-    map.addLayer(layer);
-    map.setCenter(new OpenLayers.LonLat(5, 40), 5);
+    var geojson_layer = new OpenLayers.Layer.Vector("Trails", {
+        isBaseLayer       : true,
+        strategies: [new OpenLayers.Strategy.Fixed()],
+        protocol: new OpenLayers.Protocol.HTTP({
+            url: "assets/trails.geojson",
+            format: new OpenLayers.Format.GeoJSON()
+        })
+    });
+    map.addLayer(geojson_layer);
+    map.setCenter(new OpenLayers.LonLat(-75.1667, 39.9500), 15);
 }, false);
