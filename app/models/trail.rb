ae32b03b0784ea2532effe4b0aa96b47b29a1574
Aaron Leitner <aaron.leitner14@gmail.com>
2014-06-21 12:38:16 -0400

    Fix broken migration

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,2 +1,3 @@
 class Trail < ActiveRecord::Base
+   set_rgeo_factory_for_column(:geospatial_data, RGeo::Geographic.spherical_factory(:srid => 4326))
 end
--- db/migrate/20140621153435_create_parks.rb
+++ db/migrate/20140621153435_create_parks.rb
@@ -2,12 +2,11 @@ class CreateParks < ActiveRecord::Migration
   def change
     create_table :parks do |t|
       t.string :name
-      t.string :type
-      t.string :material
-      t.integer :width
-      t.decimal :length
-      t.integer :park_id
-      t.geometry :, geographic: true
+      t.string :address
+      t.string :zipcode
+      t.decimal :acres
+      t.geometry :boundary, geographic: true
+      t.integer :district_id
       t.timestamps
     end
   end
