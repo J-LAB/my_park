97d67dbf44b18c9327427e1a786a60edd6e67c06
johnballantyne@gmail.com <john@john-ubuntu.(none)>
2014-06-21 21:40:50 -0400

    Things overlaid on a map

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,15 +1,6 @@
 
 window.addEventListener('load', function() {
-    /*
-
-    var epsg4326 = new OpenLayers.Projection('EPSG:4326');
-    var epsg900913 = new OpenLayers.Projection('EPSG:900913');
-    var lon = -75.1667;
-    var lat = 39.95;
-    var zoom = 15;
-
-    var map = new OpenLayers.Map("map");
-
+/*
     var parkStyle = new OpenLayers.StyleMap({
         "default": new OpenLayers.Style({
             fillColor: "#00FF00",
@@ -18,7 +9,7 @@ window.addEventListener('load', function() {
             strokeColor: "#00FF00",
             graphicZIndex: 3,
             graphicOpacity: 0.5,
-transparent: true
+            transparent: true
         })
     });
 
@@ -30,62 +21,15 @@ transparent: true
             graphicOpacity:1 
         })
     });
-    var layer = new OpenLayers.Layer.WMS( "OpenLayers WMS", 
-            "http://vmap0.tiles.osgeo.org/wms/vmap0",
-            {layers: 'basic'} );
-
-    var parks_layer = new OpenLayers.Layer.Vector("Parks", {
-transparent: true,
-        opacity: 0.1,
-        isBaseLayer       : false,
-        strategies: [new OpenLayers.Strategy.Fixed()],
-        protocol: new OpenLayers.Protocol.HTTP({
-            url: "assets/PPR_Boundaries.geojson",
-            format: new OpenLayers.Format.GeoJSON()
-        }),
-        styleMap: parkStyle
-    });
-
-    var trails_layer  = new OpenLayers.Layer.Vector("Trails", {
-        opacity: 0.5,
-        isBaseLayer       : false,
-        strategies: [new OpenLayers.Strategy.Fixed()],
-        protocol: new OpenLayers.Protocol.HTTP({
-            url: "assets/trails.geojson",
-            format: new OpenLayers.Format.GeoJSON()
-        }),
-    });
-
-var mapLayer = new OpenLayers.Layer.OSM();
-var google = new OpenLayers.Layer.Google("google");
-
-
-
-    map.addLayers([google, parks_layer, trails_layer]);
-//mapLayer.setVisibility(false);
-    //map.addLayer(layer);
-    map.setCenter(new OpenLayers.LonLat(lon, lat), zoom);
     */
 
     var map = new OpenLayers.Map('map', {
         projection: 'EPSG:3857',
         layers: [
-            new OpenLayers.Layer.Google(
-                "Google Physical",
-                {type: google.maps.MapTypeId.TERRAIN}
-            ),
-            new OpenLayers.Layer.Google(
-                "Google Streets", // the default
-                {numZoomLevels: 20}
-            ),
-            new OpenLayers.Layer.Google(
-                "Google Hybrid",
-                {type: google.maps.MapTypeId.HYBRID, numZoomLevels: 20}
-            ),
-            new OpenLayers.Layer.Google(
-                "Google Satellite",
-                {type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 22}
-            )
+            new OpenLayers.Layer.Google("Google Physical", {
+                visibility: false,
+                type: google.maps.MapTypeId.TERRAIN
+            })
         ],
         center: new OpenLayers.LonLat(-75.1667, 39.95)
             // Google.v3 uses web mercator as projection, so we have to
@@ -93,13 +37,41 @@ var google = new OpenLayers.Layer.Google("google");
             .transform('EPSG:4326', 'EPSG:3857'),
         zoom: 10 
     });
-    map.addControl(new OpenLayers.Control.LayerSwitcher());
-    
-    // add behavior to html
-    var animate = document.getElementById("animate");
-    animate.onclick = function() {
-        for (var i=map.layers.length-1; i>=0; --i) {
-            map.layers[i].animationEnabled = this.checked;
+
+    var trailsLayer = new OpenLayers.Layer.Vector("Trails Layer");
+    var parksLayer = new OpenLayers.Layer.Vector("Parks Layer");
+    map.addLayer(trailsLayer);
+    map.addLayer(parksLayer);
+
+    function handler(request) {
+
+        var geojson_format = new OpenLayers.Format.GeoJSON({
+            'internalProjection': map.baseLayer.projection,
+            'externalProjection': new OpenLayers.Projection("EPSG:4326")
+        });
+
+        this.layer.addFeatures(geojson_format.read(request.responseText));
+    }
+
+    var GeoJSONs = [
+        {
+            url: "assets/PPR_Boundaries.geojson",
+            layer: parksLayer
+        },
+        {
+            url:"assets/trails.geojson",
+            layer: trailsLayer
         }
-    };
+    ];        
+
+    for (var i = 0; i < GeoJSONs.length; i++) {
+        OpenLayers.Request.GET({
+            url: GeoJSONs[i].url,
+            callback: handler,
+            scope: {
+                layer: GeoJSONs[i].layer
+            }
+        });
+    }
+
 }, false);
